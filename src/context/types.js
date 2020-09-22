e02cccbd2589115da7aeae0bd324163e893d5977
Robert Laws <roblaws@me.com>
2020-09-22 10:09:08 +0300

    begin add instruction

--- src/context/types.js
+++ src/context/types.js
@@ -3,5 +3,10 @@ export const GET_ALL_SERVICE_DATA = 'GET_ALL_SERVICE_DATA';
 export const FILTER_SERVICE_DATA = 'FILTER_SERVICE_DATA';
 export const RESET_SERVICE = 'RESET_SERVICE';
 
+// Library Instruction
+export const GET_ALL_INSTRUCTION_DATA = 'GET_ALL_INSTRUCTION_DATA';
+export const FILTER_INSTRUCTION_DATA = 'FILTER_INSTRUCTION_DATA';
+export const RESET_INSTRUCTION = 'RESET_INSTRUCTION';
+
 // https://docs.google.com/spreadsheets/d/1XaaKHwXzAmr4VORHLp8cAL58ehoh4OkOr5qZQc0eD4U - service desk
-// https://docs.google.com/spreadsheets/d/1IjI7d7qDYyZrNCTn1f9-vzsbjvro-Bpgw7Xg-yXoSdU - consultations
+// https://docs.google.com/spreadsheets/d/1FEezbmJEuJ-K4Pk1XJeALsvKh1lbuMtfEiVoLz9pbGM - instruction
